614ca1ea74a52ed5814754d9a1717174cdfe9906
markup1 <709681028@qq.com>
2017-03-28 23:42:18 +0800

    Update performance_test.py
    add some discribe

--- MLSYS/NO1/performance_test.py
+++ MLSYS/NO1/performance_test.py
@@ -1,4 +1,6 @@
-
+"""
+比较几个函数的效率
+"""
 import timeit
 
 normal_py_sec = timeit.timeit('sum(x*x for x in xrange(1000))',
@@ -13,3 +15,10 @@ good_np_sec = timeit.timeit('na.dot(na)',
 print("Normal Python: %f sec" % normal_py_sec)
 print("Naive NumPy: %f sec" % naive_np_sec)
 print("Good NumPy: %f sec" % good_np_sec)
+
+"""
+Normal Python: 0.849678 sec
+Naive NumPy: 1.109494 sec
+Good NumPy: 0.018604 sec
+可以看见dot（矩阵乘法的效率是很高的）
+"""
